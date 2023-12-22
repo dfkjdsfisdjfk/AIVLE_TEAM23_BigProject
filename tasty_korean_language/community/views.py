fb4ca00b8d63e06ca0c93c41125040b9d7f26413
gogo99lddc <taier98@gmail.com>
2023-12-22 14:38:14 +0900

    views.py 수정

--- tasty_korean_language/community/views.py
+++ tasty_korean_language/community/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def test1(resquest):
-    return HttpResponse('커뮤니티')
+def test1(request):
+    tmp_list = [{'name':[1], 'content':[1]},{'name':[2], 'content':[2]},{'name':[3], 'content':[3]}]
+    
+    return render(request, 'community/community.html', {'tmp_list':tmp_list})
