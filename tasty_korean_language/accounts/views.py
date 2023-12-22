c830fb51e76e3077312153a95cba83c51eb556a4
duho <dl6874@gmail.com>
2023-12-22 13:04:02 +0900

    signup1,signup2페이지 템플릿 불러오기

--- tasty_korean_language/accounts/views.py
+++ tasty_korean_language/accounts/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def login():
     return ('hello')
 
-def signup(request):
-    return render(request, 'registration/signup.html')
+def signup1(request):
+    return render(request, 'registration/signup1.html')
+
+def signup2(request):
+    return render(request, 'registration/signup2.html')
