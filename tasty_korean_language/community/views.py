cbad9b6aac16e978bb74ceb5576ef1644f373525
gogo99lddc <taier98@gmail.com>
2023-12-22 17:08:59 +0900

    community views.py 커뮤니티 기능 추가

--- tasty_korean_language/community/views.py
+++ tasty_korean_language/community/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 def test1(request):
-    tmp_list = [{'name':[1], 'content':[1]},{'name':[2], 'content':[2]},{'name':[3], 'content':[3]}]
+    tmp_list = [{'name':[1], 'content':[1]},{'name':[2], 'content':[2]},{'name':[3], 'content':[3]}] * 3
+    page = request.GET.get('page',1)
+    paginator = Paginator(tmp_list, 1)
+    page = paginator.get_page(int(page))
     
-    return render(request, 'community/community.html', {'tmp_list':tmp_list})
+    start_page = page.number // 10 * 10
+    if page.number // 10 != paginator.num_pages // 10:
+        last_page = start_page + 10
+    else:
+        last_page = start_page + paginator.num_pages % 10
+    pages = [i for i in range(start_page+1, last_page+1)]
+    
+    contents = {
+        'page_obj' : page,
+        'page_paginator' : paginator,
+        'pages' : pages,
+    }
+
+    
+    return render(request, 'community/community.html', contents)
