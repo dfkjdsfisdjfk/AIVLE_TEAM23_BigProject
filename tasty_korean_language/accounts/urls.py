dc364c78e11ea4e860188b130c2dcebdd50042b6
dnbi <ldnb114@gmail.com>
2023-12-26 16:15:49 +0900

    로그인, 회원가입, base.html 수정

--- tasty_korean_language/accounts/urls.py
+++ tasty_korean_language/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('login/', views.login),
-     path('signup1/', views.signup1),
-     path('signup2/', views.signup2),
+     path('login/', views.login, name='login'),
+     path('signup1/', views.signup1, name='signup'),
+     path('signup2/', views.signup2, name='signup2'),
 ]
