44e6e2b87319312cf82f15b05fbff0000b581e18
duho <dl6874@gmail.com>
2023-12-26 17:16:13 +0900

    admin.py생성, user와 profile 확인 가능

--- tasty_korean_language/accounts/admin.py
+++ tasty_korean_language/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Profile, UserSession
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'address')
+    search_fields = ('user__username', 'user__email', 'phone_number', 'address')
+
+admin.site.register(Profile, ProfileAdmin)
+
+class UserSessionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'session_key', 'created_at')
+    search_fields = ('user__username', 'user__email', 'session_key')
+    readonly_fields = ('created_at',)
+
+admin.site.register(UserSession, UserSessionAdmin)
