bc76edcc0ea51090910735c655d159cf99c5fbab
duho <dl6874@gmail.com>
2023-12-26 17:25:53 +0900

    setting.py 기존 database를 mysql로 변환. REDIRECT_URL  추가

--- tasty_korean_language/tasty_korean_language/settings.py
+++ tasty_korean_language/tasty_korean_language/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = "tasty_korean_language.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'NAME': 'testdb',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
@@ -132,3 +143,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = '/accounts/profile/'
+
+LOGOUT_REDIRECT_URL = None 
+
+LOGIN_URL = '/accounts/login/'
