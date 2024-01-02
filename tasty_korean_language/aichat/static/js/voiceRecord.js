01bda8134827e5eaf292a8d3705b18bce3f5b155
dnbi <ldnb114@gmail.com>
2024-01-02 09:31:41 +0900

    aichat 음성 녹음 추가

---
+++ tasty_korean_language/aichat/static/js/voiceRecord.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    let isRecording = false;
+
+    if (navigator.mediaDevices){
+        let recordArray = []; // 녹음 데이터 저장 변수
+
+        navigator.mediaDevices.getUserMedia({audio: true})
+        .then((stream) => {
+            const mediaRecorder = new MediaRecorder(stream);
+
+            // 녹음 버튼 클릭했을 때
+            $('#record').on('click', function(){
+                if (!isRecording){
+                    mediaRecorder.start();
+                    $(this).addClass('recording');
+                    isRecording = true;
+                }
+                else {
+                    mediaRecorder.stop();
+                    $(this).removeClass('recording');
+                    isRecording = false;
+                }
+            });
+
+            mediaRecorder.onstop = (event) => {
+                // 녹음 데이터 audio 태그 추가
+                var $audio = $('<audio controls>');
+                $('#chat-box').append($('<article>').append($audio));
+
+                const blob = new Blob(recordArray, {
+                    'type': 'audio/wav codecs=opus'
+                });
+                recordArray = [];
+
+                const audioURL = URL.createObjectURL(blob);
+                $audio.attr('src', audioURL);
+
+                // 녹음 파일 저장
+                const a = document.createElement('a');
+                a.href = audioURL;
+                a.download = "record" + Date.now();
+                a.click();
+            };
+
+            // 녹음 데이터 배열에 저장
+            mediaRecorder.ondataavailable = (event) => {
+                recordArray.push(event.data);
+            };
+
+        })
+        .catch((err) => {
+            console.log(err);
+            alert(err);
+        })
+    }
+});
--- tasty_korean_language/aichat/urls.py
+++ tasty_korean_language/aichat/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [   
-               
+    path('chat/', TemplateView.as_view(template_name='aichat/chat.html'), name='chat'),  
 ]
