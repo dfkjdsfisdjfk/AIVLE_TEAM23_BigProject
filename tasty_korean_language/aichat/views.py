ed5d38367209c7dac86ac23036bf7ee40f370f4e
duho <dl6874@gmail.com>
2024-01-02 13:56:35 +0900

    chagpt API 불러오는 view랑 html 생성

--- tasty_korean_language/aichat/views.py
+++ tasty_korean_language/aichat/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from .models import ChatMessage
+
+from openai import OpenAI
+
+@login_required
+def index(request):
+    messages = ChatMessage.objects.all()
+    return render(request, 'aichat/chat.html', {'messages': messages})
+
+
+@login_required
+def send(request):
+    
+    message = request.POST.get('message')
+    user = request.user.username
+
+    # response = get_chat_gpt_response(message)
+    
+    client = OpenAI(api_key="")
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message}
+        ]
+    )
+    
+    response = completion.choices[0].message.content
+
+    message = ChatMessage(user=user, message=response)
+    message.save()
+
+    return redirect('aichat:chat')
+    
+
+
