adfa873bf00e5a233d3073e8576d5c6c113ddd7c
duho <dl6874@gmail.com>
2024-01-02 13:53:23 +0900

    아무user랑 message 저장하는 model 생성. 추후 변경

--- tasty_korean_language/aichat/admin.py
+++ tasty_korean_language/aichat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ChatMessage
 
 # Register your models here.
+
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'message', 'created_at')
+    search_fields = ('user', 'message')
+    readonly_fields = ('created_at',)
+    
+admin.site.register(ChatMessage, ChatMessageAdmin)
