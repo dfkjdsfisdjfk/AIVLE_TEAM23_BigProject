d7c8d99ce15b674cd5b5f3905f79285eafd102c2
duho <dl6874@gmail.com>
2024-01-02 13:55:44 +0900

    app_name 지정하고 path 생성. 이에 따라서 base.html에서도 url 바꿔줌

--- tasty_korean_language/aichat/urls.py
+++ tasty_korean_language/aichat/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views 
+
+app_name = 'aichat'
 
 urlpatterns = [   
-    path('chat/', TemplateView.as_view(template_name='aichat/chat.html'), name='chat'),  
+    # path('chat/', TemplateView.as_view(template_name='aichat/chat.html'), name='chat'),  
+    path('', views.index, name='chat'),
+    path('send/', views.send, name='send'),
 ]
