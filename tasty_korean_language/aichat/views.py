44070e53f1e92ca4319d2041bd875518b4432217
dnbi <ldnb114@gmail.com>
2024-01-08 09:05:08 +0900

    aichat 녹음 파일 전송, 재녹음 추가

--- tasty_korean_language/aichat/views.py
+++ tasty_korean_language/aichat/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, resolve_url
 from django.http import JsonResponse
+from django.conf import settings
+from django.core.files.storage import default_storage
 
 from django.contrib.auth.decorators import login_required
 
@@ -11,7 +13,8 @@ from openai import OpenAI
 def index(request):
     chatlog = ChatLog.objects.create(user=request.user)
     
-    return render(request, 'aichat/chat.html', {'messages': '', 'id': chatlog.id})
+    return redirect('aichat:chatlog', chatlog.id)
+    # return render(request, 'aichat/chat.html', {'messages': '', 'id': chatlog.id})
 
 
 @login_required
@@ -24,16 +27,18 @@ def index2(request, id):
 
 @login_required
 def send(request, id):
-    
+    audio = request.FILES.get('audio')
     message = request.POST.get('message')
     sender = request.user.username
+    
+    file_path = default_storage.save(sender+'/audio.mp3', audio)
     
     chatlog = ChatLog.objects.get(id=id)
     
     ChatMessage.objects.create(chatlog=chatlog, sender=sender, message=message)
     # response = get_chat_gpt_response(message)
     
-    client = OpenAI(api_key="")
+    client = OpenAI(api_key=settings.SECRET_API_KEY)
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
