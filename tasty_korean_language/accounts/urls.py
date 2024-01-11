2d29a0bc130606022db6fb278c76690fbb6d6227
duho <dl6874@gmail.com>
2024-01-11 16:38:22 +0900

    main 페이지에서만 배경화면 다르도록 변경

--- tasty_korean_language/accounts/urls.py
+++ tasty_korean_language/accounts/urls.py
@@ -24,9 +24,9 @@ urlpatterns = [
      path("password_reset_done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
      path("password_reset_confirm//<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
      path("password_reset_complete/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
-    path('cookie/<code>/', views.cookie_test),
-    
-    path('session/<int:code>', views.session_test),
+     path('cookie/<code>/', views.cookie_test),
+     
+     path('session/<int:code>', views.session_test),
      
 #     path('mypage/', TemplateView.as_view(template_name='registration/mypage.html'), name='mypage'),
 
